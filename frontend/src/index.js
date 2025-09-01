2d03da0ca4c7633f1eb547d6fc91721341e856f2
StalagLuft3 <thomas11326@gmail.com>
2025-09-01 01:23:14 +0100

    Frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import "@ukic/fonts/dist/fonts.css";
+import "@ukic/web-components/dist/core/core.css";
+import "@ukic/web-components/dist/core/normalize.css";
+
+import { defineCustomElements } from "@ukic/web-components/loader";
+defineCustomElements();
